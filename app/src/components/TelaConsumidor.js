22526031bcf275ac1d29f5fe737848f00ed65a32
Rosana Rezende <rezende_rosana@hotmail.com>
2020-03-12 11:22:35 -0300

    Ordenação

--- app/src/components/TelaConsumidor.js
+++ app/src/components/TelaConsumidor.js
@@ -50,8 +50,8 @@ class TelaConsumidor extends React.Component {
 			categoriaAtualState: '',
 			idCardAtivo:'',
 			filtroMin: '',
-			filtroMax: ''
-
+			filtroMax: '',
+			ordem: ''
 		}
 	}
 
@@ -106,11 +106,52 @@ class TelaConsumidor extends React.Component {
 		})
 	}
 
+	mudaOrdenacao = (event) => {
+		this.setState({
+			ordem: event.target.value
+		})
+	}
+
 	render() {
 
 		let listaOrdenada
+		if (this.state.ordem === 'crescente') {
+			listaOrdenada = this.state.listaDeProdutosState.sort(function (a, b) {
+				return a.price < b.price ? -1 : a.price > b.price ? 1 : 0
+			})
+		} else if (this.state.ordem === 'decrescente') {
+			listaOrdenada = this.state.listaDeProdutosState.sort(function (a, b) {
+				return a.price < b.price ? 1 : a.price > b.price ? -1 : 0
+			})
+		} else if (this.state.ordem === 'nomeZA') {
+			listaOrdenada = this.state.listaDeProdutosState.sort(function (a, b) {
+				return a.name.toLowerCase() < b.name.toLowerCase() ? 1 : a.name.toLowerCase() > b.name.toLowerCase() ? -1 : 0
+			})
+		} else if (this.state.ordem === 'nomeAZ') {
+			listaOrdenada = this.state.listaDeProdutosState.sort(function (a, b) {
+				return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : a.name.toLowerCase() > b.name.toLowerCase() ? 1 : 0
+			})
+		}
+		else if (this.state.ordem === 'categoriaZA') {
+			listaOrdenada = this.state.listaDeProdutosState.sort(function (a, b) {
+				return a.category < b.category ? 1 : a.category > b.category ? -1 : 0
+			})
+		} else if (this.state.ordem === 'categoriaAZ') {
+			listaOrdenada = this.state.listaDeProdutosState.sort(function (a, b) {
+				return a.category < b.category ? -1 : a.category > b.category ? 1 : 0
+			})
+		}
 
-		let listaNaoFiltrada = this.state.listaDeProdutosState.map((produto, index) => (
+
+let lista
+		if (listaOrdenada) {
+			lista = listaOrdenada
+		} else {
+			lista = this.state.listaDeProdutosState
+		}
+
+
+		let listaNaoFiltrada = lista.map((produto, index) => (
 				<ConteudoCartao
 								key={index}
 								id={produto.id}
@@ -126,13 +167,13 @@ class TelaConsumidor extends React.Component {
 							/>
 		))
 
-		let listaFiltrada = this.state.listaDeProdutosState.filter(cadaProduto => {
+		let listaFiltrada = lista.filter(cadaProduto => {
 			let filtroBusca  = this.props.inputPesquisa
 			let filtroMin = this.state.filtroMin
 			let filtroMax = this.state.filtroMax
 			let categoria = this.state.categoriaAtualState
 
-			let valorDoProduto = cadaProduto.price
+			let valorDoProduto = Number(cadaProduto.price)
 
 			// 	TODOS OS FILTROS
 			if (filtroBusca && filtroMin && filtroMax && categoria) {
@@ -285,33 +326,28 @@ class TelaConsumidor extends React.Component {
 							variant="outlined"
 						/>
 						<TextField
-							// error={this.state.inputDescricaoOK}
-							label="Ordernar Por:"
-							// value={this.state.inputDescricao}
-							// onChange={}
+							select
+							label="Ordenar por"
+							name='ordem'
+							value={this.state.ordem}
+							onChange={this.mudaOrdenacao}
+							SelectProps={{
+								native: true,
+							}}
 							margin="normal"
 							variant="outlined"
-						/>
-					{/* 	<TextField
-						select
-						required
-						label="Categoria do Produto"
-						name='inputCategoria'
-						value={this.state.inputCategoria}
-						onChange={this.atualizaValorEntrada}
-						SelectProps={{
-							native: true, 
-						}}
-						margin="normal"
-						variant="outlined"
-					>
-						<option hidden value=''></option>
-						<option value={'roupas'}>Roupas</option>
-						<option value={'artigosDeDecoracao'}>Artigos de decoração</option>
-						<option value={'calcados'}>Calçados</option>
-						<option value={'eletronicos'}>Eletrônicos</option>
-						<option value={'moveis'}>Móveis</option>
-					</TextField> */}
+						>
+							<option hidden value=''></option>
+							<option value={'crescente'}>Menor Preço</option>
+							<option value={'decrescente'}>Maior Preço</option>
+							<option value={'nomeAZ'}>Nome (A -Z)</option>
+							<option value={'nomeZA'}>Nome (Z - A)</option>
+							<option value={'categoriaAZ'}>Categoria (A -Z)</option>
+							<option value={'categoriaZA'}>Categoria (Z - A)</option>
+							
+							{/* <option value={'moveis'}>Móveis</option> */}
+</TextField>
+					
 					</ValuesContainer>
 
 					<CardsContainer>
